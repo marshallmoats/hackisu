bcb5884ff24fdd477a07a9283d3fe36ef5af9606
Thanh <kewber0@gmail.com>
2023-09-30 17:55:31 -0500

    synch changes

--- webapp/src/components/MarketplacePage/MarketMainContent.tsx
+++ webapp/src/components/MarketplacePage/MarketMainContent.tsx
@@ -1,14 +1,17 @@
 import Box from "@mui/material/Box";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useContext } from "react";
+import { AppContext } from "../../context";
 
 interface MarketMainContentProps {
 
 }
 
 const MarketplaceMainContent: FunctionComponent<MarketMainContentProps> = (props): JSX.Element => {
+    const appCtx = useContext(AppContext);
 
     return (<Box className="market-main-content">
-
+        
+        {JSON.stringify(appCtx)}
     </Box>);
 }
 
--- webapp/src/components/MarketplacePage/MarketplacePage.tsx
+++ webapp/src/components/MarketplacePage/MarketplacePage.tsx
@@ -1,11 +1,12 @@
 import "./styles.css";
 
 import Box from "@mui/material/Box";
-import { FunctionComponent, useContext, useEffect } from "react";
+import { FunctionComponent, useContext, useEffect, useState } from "react";
 import MarketLeftSidebar from "./MarketLeftSidebar";
 import MarketMainContent from "./MarketMainContent";
 import { AppContext } from "../../context";
 import { getMarketList } from "../../utils/BackendCalls";
+import { MarketProps } from "../../utils/types";
 
 interface MarketplacePageProps {
 
@@ -13,10 +14,14 @@ interface MarketplacePageProps {
 
 const MarketplacePage: FunctionComponent<MarketplacePageProps> = (props): JSX.Element => {
     const appCtx = useContext(AppContext);
+    const [markets, setMarkets] = useState<MarketProps[]>([]);
 
     useEffect(() => {
-        getMarketList().then((res) => {
-            appCtx.markets = res ?? appCtx.markets
+        getMarketList().then((res): void => {
+            setMarkets(res ?? markets);
+            appCtx.markets = markets;
+            appCtx.setMarkets = setMarkets;
+            console.log(markets)
         })
     }, [])
 
