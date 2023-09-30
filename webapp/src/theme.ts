3860c5430f7549877ba89c425cb7b2cb06de8779
Thanh <kewber0@gmail.com>
2023-09-30 15:37:29 -0500

    menu bar set up

---
+++ webapp/src/theme.ts
@@ -0,0 +1,31 @@
+import { createTheme } from '@mui/material/styles';
+
+const globalTheme = createTheme({
+    palette: {
+        background: {
+            default: '#f5eadc',
+        },
+        primary: {
+            main: '#c75d16',
+        },
+    },
+
+    typography: {
+        fontSize: 13,
+        fontFamily: [
+            'Barlow',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(",")
+    }
+});
+
+export default globalTheme;
